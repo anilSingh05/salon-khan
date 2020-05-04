3d043c354ff38eb2866d98fd61f270637c3bc67c
Anil Singh <anil05.me@gmail.com>
2020-05-04 23:52:53 +0530

    Initial Commit

---
+++ src/main/java/com/agro/mobile/salonkhanapp/SalonKhanAppApplication.java
@@ -0,0 +1,13 @@
+package com.agro.mobile.salonkhanapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalonKhanAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalonKhanAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agro/mobile/salonkhanapp/SalonKhanAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agro.mobile.salonkhanapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalonKhanAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
