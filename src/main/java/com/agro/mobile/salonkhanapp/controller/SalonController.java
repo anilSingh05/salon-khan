0bb015a24b4febd3faf64c9b330a47e9b25abc5a
Anil Singh <anil05.me@gmail.com>
2020-05-05 00:28:14 +0530

    Update input

--- src/main/java/com/agro/mobile/salonkhanapp/controller/SalonController.java
+++ src/main/java/com/agro/mobile/salonkhanapp/controller/SalonController.java
@@ -5,14 +5,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SalonController {
-    @GetMapping(value = "/salonkhan/welcome", produces = {MediaType.APPLICATION_JSON_VALUE})
-    public ResponseEntity<Greetings> greetings(){
+    @GetMapping(value = "/salonkhan/{name}", produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<Greetings> greetings(@PathVariable("name") String name){
         Greetings message = Greetings.builder()
-                .message("Welcome To SalonKhan").build();
+                .message(name +" : Welcome To SalonKhan").build();
         return new ResponseEntity<>(message, HttpStatus.OK);
     }
 }
